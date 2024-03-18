e20fba064bde68c4a478b4ef93fdc5897838ef28
hawlermathew2007 <hawlermathewgit@gmail.com>
2024-03-18 18:45:22 +0700

    Finish it again

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import TagManager from 'react-gtm-module';
 import App from './App.jsx'
 import './index.css'
+
+TagManager.initialize({
+  gtmId: 'GTM-T8RD7MSN',
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
